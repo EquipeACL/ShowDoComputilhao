65e8ab70ca8a3f4bb643ee04fe7dc533a5d79b59
Adalcino Junior <adaljunior.inf@gmail.com>
2018-09-22 18:10:12 -0300

    Adicionado os template das paginas

--- show-do-milhao-ui/src/app/app.module.ts
+++ show-do-milhao-ui/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { DetalhesJogadorComponent } from './game/detalhes-jogador/detalhes-jogador.component';
+import { PerguntaComponent } from './game/pergunta/pergunta.component';
+import { RankComponent } from './game/rank/rank.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    DetalhesJogadorComponent,
+    PerguntaComponent,
+    RankComponent
   ],
   imports: [
     BrowserModule
---
+++ show-do-milhao-ui/src/app/game/detalhes-jogador/detalhes-jogador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes-jogador',
+  templateUrl: './detalhes-jogador.component.html',
+  styleUrls: ['./detalhes-jogador.component.css']
+})
+export class DetalhesJogadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
